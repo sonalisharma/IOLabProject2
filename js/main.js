309bae7788f7716a2c07b07f5abaeddcfc0f96bb
Arthur Che <che@ischool.berkeley.edu>
2012-10-06 11:03:43 -0700

    started laying out pseudocode

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// On document ready, function for button to initiate API call to Flickr, return JSON of image links?
+
+// Create draggable initial image gallery
+
+// Function to create and display circles
+
+// Function for spellcheck of new circle names
+
+// Function for dropping images into circles
